2437ab907f79ded86f44c7afb0c352b3efeba3ef
AtElias23 <128533368+AtElias23@users.noreply.github.com>
2023-07-10 17:20:48 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 document.getElementById("header").innerHTML=` 
 <nav class="d-block p-2 navbar navbar-expand-sm navbar-dark bg-dark border-bottom">
-<div class="container"><a class="navbar-brand" href="/templates/productos.html">CRUD</a><a class="navbar-brand" href="index.html">SALIR</a></div>
+<div class="container"><a class="navbar-brand" href="/templates/productos.html">CRUD</a><a class="navbar-brand" href="/index.html">SALIR</a></div>
 </nav>`
